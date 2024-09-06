bf0574ed8976c9a81b9b7407e200686d2e487d08
Evgeny Shut <evgeny.shut1@gmail.com>
2024-09-06 14:49:56 +0200

    Update logger.py | Add exception hook handler

--- framework/logger/logger.py
+++ framework/logger/logger.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import time
+import sys
 
 from framework.common import jsonGetter
 
@@ -10,22 +11,21 @@ BROWSER = jsonGetter.GetJson.get_file(CONFIG, "actualBrowser")
 
 class Logger:
     def __init__(self, logger):
+        self.log_level = constants.LOG_LEVEL
         self.logger = logging.getLogger(os.path.basename(logger))
-        self.logger.setLevel(logging.DEBUG)
-        if os.path.exists("logs"):
-            pass
-        else:
+        self.logger.setLevel(self.log_level)
+        if not os.path.exists("logs"):
             os.mkdir("logs")
 
-        log_time = time.strftime(BROWSER + '-%Y-%m-%d-%H%M-%S', time.localtime(time.time()))
+        log_time = time.strftime(TOKEN_LOG + '-%Y-%m-%d-%H%M-%S', time.localtime(time.time()))
         log_path = os.getcwd() + "/logs/"
         log_name = log_path + log_time + '.log'
 
-        fh = logging.FileHandler(log_name)
-        fh.setLevel(logging.INFO)
+        fh = logging.FileHandler(log_name, encoding="utf-8")
+        fh.setLevel(self.log_level)
 
         ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
+        ch.setLevel(self.log_level)
 
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         fh.setFormatter(formatter)
@@ -34,5 +34,20 @@ class Logger:
         self.logger.addHandler(fh)
         self.logger.addHandler(ch)
 
+        sys.excepthook = self.handle_exception
+
+    def handle_exception(self, exc_type, exc_value, exc_traceback):
+        """Handles uncaught exceptions"""
+        if issubclass(exc_type, KeyboardInterrupt):
+            # Call the default hook if KeyboardInterrupt
+            sys.__excepthook__(exc_type, exc_value, exc_traceback)
+            return
+
+        self.logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
     def get_log(self):
         return self.logger
+
+    def error(self, msg, *args, **kwargs):
+        self.logger.error(msg, *args, **kwargs)
+
